11019bc55b7a890547994c61f06f53d68da01a43
ExEdzy <edgars.liepa@creo.mobi>
2011-04-27 17:19:02 +0300

    statistic - atlase = ok

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -15,14 +15,13 @@ class StatisticController < ApplicationController
         user.users_sticks.each do |user_stick|
           puts "  - stick: #{ user_stick.stick.stick_type } (id: #{user_stick.stick.id}) "
           
-          #@PairHits = user_stick.pair_hit
           @PairHits = user_stick.pair_hits.all
           
-          
           @PairHits.each do |pair_hit|
-          #puts "      pair hit id: #{ pair_hit.id }"
+            puts "      pair hit id: #{ pair_hit.id }"
+            puts "        pair hit planed hit id: #{ pair_hit.hit_planed.id }"
+            puts "        pair hit real hit id: #{ pair_hit.hit_real.id }"
           end
-
 
         end # user stick
         
