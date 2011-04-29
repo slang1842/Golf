9af4f720409e56faf434df7e675fcbbdfa6c03ac
ExEdzy <edgars.liepa@creo.mobi>
2011-04-29 11:34:50 +0300

    small statistics update

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -16,197 +16,47 @@ class Statistic < ActiveRecord::Base
   
   
   def self.calculate_statistics
-    @users = User.all    
-    puts "======================"
+    @users = User.all
     
-    # user
+    #users starts
     @users.each do |user|
-      puts "* user: #{ user.email } (id: #{user.id} )  "
-        
-        # user stick
-        user.users_sticks.each do |user_stick|
-          puts "  - stick: #{ user_stick.stick.stick_type } (id: #{user_stick.stick.id}) "
-          
-          
-          @PairHits = user_stick.pair_hits.all
-          
-          
-          
-            @PairHits.each do |pair_hit|
-              puts "      pair hit id: #{ pair_hit.id }"
-            
-            
-            #STATISTIC ==============================================
-            
-            
-            
-            puts ""
-            puts ""
-            puts "-----------------------------"
-            
-              @HP = Hit.find(pair_hit.hit_planed.id)
-              @HR = Hit.find(pair_hit.hit_real.id)
-            
-              puts "        pair hit planed hit id: #{ @HP.id }"
-              puts "        pair hit real hit id: #{ @HR.id }"
-            
-            puts " xxxx #{PairHit.count}"
-            
-            
-            
-            
-            
-            #place ===================================================
-            place_from = @HP.place_from
-            if place_from == 1
-              puts "          - Teebox"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            elsif place_from == 2
-              puts "          - feairway"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            elsif place_from == 3
-              puts "          - Next fairway"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            elsif place_from == 4
-              puts "          - Semi raf"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            elsif place_from == 5
-              puts "          - Raf"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            elsif place_from == 6
-              puts "          - For green"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            elsif place_from == 7
-              puts "          - Green"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            elsif place_from == 8
-              puts "          - Fairway sand"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            elsif place_from == 9
-              puts "          - Green sand"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            elsif place_from == 10
-              puts "          - Wood"
-            elsif place_from == 11
-              puts "          - From water"
-              puts @HP.hit_distance
-              puts @HR.hit_distance
-              
-            else
-              puts "          - N"
-            end
-            #place ===================================================
-             
-            #stance ===================================================
-            stance = pair_hit.hit_planed.stance
-            if stance == 1
-              puts "          - Normal"
-            elsif stance == 2
-              puts "          - Right leg lower"
-            elsif stance == 3
-              puts "          - Left leg lower"
-            elsif stance == 4
-              puts "          - Ball lower"
-            elsif stance == 5
-              puts "          - Ball higher"
-            else
-              puts "          - N"
-            end
-            #place ===================================================
-             
-            #direction ==================================================
-            direction = pair_hit.hit_planed.direction
-            if direction == 1
-              puts "          - Straigth"
-            elsif direction == 2
-              puts "          - Fade"
-            elsif direction == 3
-              puts "          - Drow"
-            elsif direction == 4
-              puts "          - Slice"
-            elsif direction == 5
-              puts "          - Hook"
-            else
-              puts "          - N"
-            end
-            #direction ===================================================
-             
-            #temperature ==================================================
-            temperature = Game.find(pair_hit.hit_planed.game_id).temperature
-            if temperature == 3
-              puts "          - Cold"
-            elsif temperature == 2
-              puts "          - Normal"
-            elsif temperature == 1
-              puts "          - Hot"
-            else
-              puts "          - N"
-            end
-            #temperature ===================================================
-            
-             #weather ==================================================
-            weather = Game.find(pair_hit.hit_planed.game_id).weather
-            if weather == 1
-              puts "          - Normal"
-            elsif weather == 2
-              puts "          - Wind"
-            elsif weather == 3
-              puts "          - Rain"
-            elsif weather == 4
-              puts "          - Wind and Rain"
-            else
-              puts "          - N"
-            end
-            #weather ===================================================
-             
-             #trajectory ==================================================
-            trajectory = pair_hit.hit_planed.trajectory
-            if trajectory == 1
-              puts "          - Normal"
-            elsif trajectory == 2
-              puts "          - High"
-            elsif trajectory == 3
-              puts "          - Low"
-            else
-              puts "          - N"
-            end
-            #trajectory ===================================================
-             
-            
-            
-            # END STATISTIC ===============================================
-            
-
-            
-          end
-
-        end # user stick
-        
-      puts ""
-      puts ""
-      puts ""
-      puts "======================"
+    puts " user id: #{user.id}"
     
-    end # user
+      #user sticks starts
+      user.users_sticks.each do |user_stick|
+      puts "  user stick id:#{user_stick.id}"
+      
+      #-----------------------------
+      ph_count = PairHit.count
+      
+        # for i in 1..ph_count.to_i STARTS
+        for i in 1..ph_count.to_i
+          puts "   i: #{i}"
+          
+              #each pair hit starts
+              @get_pair_hit = Hit.where(:pair_id => i)
+
+              @get_pair_hit.each do |get_hit|
+                
+                puts "     get_pair_hit #{get_hit.id}"
+                
+              #each pair hit ends
+              end
+          
+        # for i in 1..ph_count.to_i ENDS
+        end
+      
+      
+      
+      
+      #-----------------------------
+      
+      #user sticks ends
+      end
     
+    
+    #users ends
+    end
     
   end
 end
