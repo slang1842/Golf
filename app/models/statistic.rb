fe73e71b53dbcba3e7506d0c52bdd11de3d5d9b5
ExEdzy <edgars.liepa@creo.mobi>
2011-05-05 09:42:31 +0300

    s

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -55,8 +55,7 @@ def self.calculate_statistics
                @result_arr.push(calculate_current_statistics(each_pair.hit_planed.hit_distance, each_pair.hit_real.hit_distance))
             end
           end
-         
-         
+
           case place_from_num 
             when 1
               if @result_arr.size != 0
@@ -118,6 +117,49 @@ def self.calculate_statistics
         # ==========================================
         
         
+        #CALCULATE STANCE
+        # ==========================================
+        for stance_num in 1..5 do
+          @all_pairs = PairHit.where(:users_id => user.id)
+          
+          @result_arr = []
+        
+          @all_pairs.each do |each_pair|
+            if each_pair.hit_planed.stance == stance_num && each_pair.hit_planed.stick_id == user_stick.stick.id
+               @result_arr.push(calculate_current_statistics(each_pair.hit_planed.hit_distance, each_pair.hit_real.hit_distance))
+            end
+          end
+
+          case stance_num 
+            when 1
+              if @result_arr.size != 0
+                puts "    stance: Normal: #{(@result_arr.inject(0.0) { |sum, el| sum + el } / @result_arr.size).to_i}%"
+                puts "   ---"
+              end
+            when 2
+              if @result_arr.size != 0
+                puts "    stance: Right leg lower: #{(@result_arr.inject(0.0) { |sum, el| sum + el } / @result_arr.size).to_i}%"
+                puts "   ---"
+              end
+            when 3
+              if @result_arr.size != 0
+                puts "    stance: Left leg lower: #{(@result_arr.inject(0.0) { |sum, el| sum + el } / @result_arr.size).to_i}%"
+                puts "   ---"
+              end
+            when 4
+              if @result_arr.size != 0
+                puts "    stance: Ball lower: #{(@result_arr.inject(0.0) { |sum, el| sum + el } / @result_arr.size).to_i}%"
+                puts "   ---"
+              end
+            when 5
+              if @result_arr.size != 0
+                puts "    stance: Ball higher: #{(@result_arr.inject(0.0) { |sum, el| sum + el } / @result_arr.size).to_i}%"
+                puts "   ---"
+              end
+          end
+        end
+        # ==========================================
+        
         
       end
     end
@@ -126,31 +168,4 @@ def self.calculate_statistics
   end
   
 
-  
-  def calculate_place_from
-    case place_from_num 
-      when 1
-      puts "    Teebox"
-      when 2
-      puts "    Feairway"
-      when 3
-        puts "    Next fairway"
-      when 4
-        puts "    Semi raf"
-      when 5
-        puts "    Raf"
-      when 6
-        puts "    For green"
-      when 7
-        puts "    Green"
-      when 8
-        puts "    Fairway sand"
-      when 9
-        puts "    Green sand"
-      when 11
-        puts "    Wood"
-      when 11
-        puts "    From water"
-    end
-  end
 end
