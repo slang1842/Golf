e65dfc7a3d1b823755f35eb56a7acb2530c043fb
ExEdzy <edgars.liepa@creo.mobi>
2011-05-06 13:53:19 +0300

    admin panel fixes, super_admin, user filters in page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,18 +1,13 @@
 class AdminController < ApplicationController
-  before_filter :require_super_admin
-  skip_before_filter :require_no_super_admin
+  before_filter :require_super_admin, :except => :index
+  skip_before_filter :require_no_super_admin#, :except => :index
+  skip_before_filter :require_user, :only => :index
+  before_filter :require_user, :only => :index
   
-   /
+  
   def index
     
   end
   
-  private
- 
-  def authenticate_http
-    authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == USER_NAME && password == PASSWORD
-    end
-  end
-  /
+
 end
