cbc58c2d12b7f57323477f584fdfbdcbcba3c406
ExEdzy <edgars.liepa@creo.mobi>
2011-05-06 13:15:12 +0300

    admin style, added logout button

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,17 +1,18 @@
 class AdminController < ApplicationController
   before_filter :require_super_admin
-
+  skip_before_filter :require_no_super_admin
   
-  
+   /
   def index
     
   end
   
   private
-  
+ 
   def authenticate_http
     authenticate_or_request_with_http_basic do |user_name, password|
       user_name == USER_NAME && password == PASSWORD
     end
   end
+  /
 end
--- app/models/users_stick.rb
+++ app/models/users_stick.rb
@@ -3,5 +3,5 @@ class UsersStick < ActiveRecord::Base
   belongs_to :stick
   belongs_to :hits
   has_many   :pair_hits
-  validates_presence_of :distance, :degrees, :shaft, :shaft_strength, :message => "fill all fields"
+  validates_presence_of :distance, :degrees, :shaft, :shaft_strength
 end
