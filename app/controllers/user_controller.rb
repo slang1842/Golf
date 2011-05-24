9f7e2f23edcdeb0c2e30ff521e4d424720dfb365
ExEdzy <edgars.liepa@creo.mobi>
2011-05-24 15:53:11 +0300

    updates

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
   def login_or_register                    
   end                                      
 
-   def new                                 
+  def new                                 
     @user = User.new 
     store_location
   end                                      
@@ -18,33 +18,58 @@ class UserController < ApplicationController
 
   def create                               
     @user = User.new(params[:user])        
-      if @user.save                        
-       redirect_back_or_default(welcome_path)
-      else                                 
-       render :action => "new"             
-      end                                  
+    if @user.save                        
+      redirect_back_or_default(welcome_path)
+    else                                 
+      render :action => "new"             
+    end                                  
   end                                      
 
   def update
-      #====================================
-      @params = params[:user]
-      unless @params[:golf_club] == current_user.golf_club
-        current_user.update_attributes(:admin => false)
+    #====================================
+    @add_club
+    golf_params = params[:user]
+    golf_params = golf_params[:golf_club]
+    
+    /
+    case golf_params
+    when true
+      @add_club = true
+      golf_params = nil
+    when false
+      golf_params = nil
+      @add_club = false
+    when current_user.golf_club
+    else
+      current_user.update_attributes(:admin => false)
+    end
+    
+    /
+    if golf_params == true
+      #is true
+      @add_club = true
+    else
+      golf_params = nil
+      current_user.update_attributes(:admin => false)
+    end unless golf_params == current_user.golf_club
+   
+    @user = current_user                 
+    if @user.update_attributes(params[:user])
+      if @add_club
+        redirect_to new_golf_club_path
+      else
+        redirect_to welcome_path
       end
-      #====================================
-      @user = current_user                 
-      if @user.update_attributes(params[:user])
-        redirect_to welcome_path           
-      else      
-        respond_to do |format|
-          format.html { render :action => "edit" }
-        end
+    else      
+      respond_to do |format|
+        format.html { render :action => "edit" }
+      end
     end                                    
   end   
   
    
   #============================================
-   def bag
+  def bag
     # bag
     @user_sticks_id = :get_value
     
@@ -73,18 +98,18 @@ class UserController < ApplicationController
     @balls_attributes = "balls_attributes[]"
     @new_balls_attributes = (@balls.count > 0) ? "new_balls_attributes[]" : @balls_attributes
     
-       params[:user] = merge_hash(params[:user], "users_sticks_attributes", "new_users_sticks_attributes")
-       params[:user] = merge_hash(params[:user], "balls_attributes", "new_balls_attributes")
+    params[:user] = merge_hash(params[:user], "users_sticks_attributes", "new_users_sticks_attributes")
+    params[:user] = merge_hash(params[:user], "balls_attributes", "new_balls_attributes")
 
       
-      respond_to do |format|
-        if @user.update_attributes(params[:user])
-          format.html { redirect_to bag_user_path }
-          format.xml  { head :ok }
-        else
-          format.html { render :action => "bag" }
-          format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to bag_user_path }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "bag" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
     end  
   end
 end
