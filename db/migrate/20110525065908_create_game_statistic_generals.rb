a2dc669cc15de56fd0138785dd8662ff0e17ae0e
ExEdzy <edgars.liepa@creo.mobi>
2011-05-25 10:07:35 +0300

    game_statistic_general update

--- db/migrate/20110525065908_create_game_statistic_generals.rb
+++ db/migrate/20110525065908_create_game_statistic_generals.rb
@@ -1,7 +1,10 @@
 class CreateGameStatisticGenerals < ActiveRecord::Migration
   def self.up
     create_table :game_statistic_generals do |t|
-
+      t.references :game
+      t.integer    :hit_sum
+      t.tinteger   :put_sum
+      t.integer    :gir_sum
       t.timestamps
     end
   end
