f4ccd24a78bf694ac2c620af681468b30099c13b
ExEdzy <edgars.liepa@creo.mobi>
2011-05-31 09:38:39 +0300

    main statistic validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,90 +6,90 @@ class ApplicationController < ActionController::Base
   before_filter :is_blocked
   
   private
-    def is_blocked
-      if current_user
-        if current_user.is_blocked
-          redirect_to logout_path
-          return true
-        else
-          return false
-        end
-      end
-    end
-    
-    def store_location
-      session[:return_to] = request.fullpath
-    end
-    
-    def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
-    end
-
-    def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.user
-    end
-    
-     def require_user
-      unless current_user
-        store_location
-        flash.now[:notice] = "You must be logged in to access this page"
-        redirect_to login_or_register_path
+  def is_blocked
+    if current_user
+      if current_user.is_blocked
+        redirect_to logout_path
+        return true
+      else
         return false
       end
     end
+  end
     
-    def require_super_admin
-      if current_user
-        unless current_user.is_super_admin
-          redirect_to welcome_path
-        end
-      else
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+    
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.user
+  end
+    
+  def require_user
+    unless current_user
+      store_location
+      flash.now[:notice] = "You must be logged in to access this page"
+      redirect_to login_or_register_path
+      return false
+    end
+  end
+    
+  def require_super_admin
+    if current_user
+      unless current_user.is_super_admin
+        redirect_to welcome_path
+      end
+    else
+      redirect_to welcome_url
+    end
+  end
+    
+  def require_no_super_admin
+    if current_user
+      if current_user.is_super_admin
+        current_user_session.destroy
         redirect_to welcome_url
       end
     end
+  end
     
-    def require_no_super_admin
-      if current_user
-        if current_user.is_super_admin
-          current_user_session.destroy
-          redirect_to welcome_url
-        end
+    
+  def require_admin
+    if require_user
+      unless current_user.admin
+        flash.now[:notice] = "You have no access this page"
+        redirect_to welcome_path
       end
     end
+  end
     
-    
-    def require_admin
-      if require_user
-        unless current_user.admin
-          flash.now[:notice] = "You have no access this page"
-          redirect_to welcome_path
-        end
-      end
+  def require_no_user
+    logger.debug "ApplicationController::require_no_user"
+    if current_user
+      store_location
+      flash.now[:notice] = "You must be logged out to access this page"
+      redirect_to loged_in_path
+      return false
     end
-    
-    def require_no_user
-      logger.debug "ApplicationController::require_no_user"
-      if current_user
-        store_location
-        flash.now[:notice] = "You must be logged out to access this page"
-        redirect_to loged_in_path
-        return false
-      end
-    end
+  end
 
-    def redirect_back
-      redirect_to(session[:return_to])
-      session[:return_to] = nil
-    end
+  def redirect_back
+    redirect_to(session[:return_to])
+    session[:return_to] = nil
+  end
     
-    def redirect_back_or_default(default)
-      redirect_to(session[:return_to] || default)
-      session[:return_to] = nil
-    end
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
     
-   / def require_owner
+  / def require_owner
       unless params[:id].to_i == current_user.id
           store_location
           redirect_to welcome_path
@@ -98,20 +98,25 @@ class ApplicationController < ActionController::Base
     end/
     
     
-      def merge_hash params, attributes_name, new_attributes_name
-        if params && params.include?(new_attributes_name)
-          params[attributes_name] = {} unless params[attributes_name]
-          max = params[attributes_name].keys.max.to_i
-          params[new_attributes_name].each do |new_attributes|
-            max = max + 1
-            params[attributes_name][max] = new_attributes
-          end
-          params.delete(new_attributes_name)
-        end
-        return params
+  def merge_hash params, attributes_name, new_attributes_name
+    if params && params.include?(new_attributes_name)
+      params[attributes_name] = {} unless params[attributes_name]
+      max = params[attributes_name].keys.max.to_i
+      params[new_attributes_name].each do |new_attributes|
+        max = max + 1
+        params[attributes_name][max] = new_attributes
       end
+      params.delete(new_attributes_name)
+    end
+    return params
+  end
       
-      
-   
+  def render_404
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
+      format.xml { head :not_found }
+      format.any { head :not_found }
+    end
+  end
 
 end
--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -1,6 +1,6 @@
 class StatisticController < ApplicationController
   skip_before_filter :require_user
- 
+   
   def statistics
     redirect_to view_statistic_path if Statistic.check_golf_club_pay_banner_time_limit && Statistic.main_statistics && Statistic.all_sticks_statistics && Statistic.game_statistics_by_sticks && Statistic.game_statistics_by_holes
   end
@@ -12,27 +12,18 @@ class StatisticController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:user_id])
-    @is_admin = true if @user.admin == true
+    @user_params = User.find(params[:user_id])
+    @is_admin = true if current_user.admin
 
-    
-    @user_fields = Field.where(:golf_club_id => @user.golf_club.id)
-
-    
-    redirect_to main_statistic_path(:user_id => current_user.id, :field_id => Field.find(current_user.golf_club.id)) unless @is_admin == false && @user.id != current_user.id
-    #sataisit redirect
-    #pielikt klat field kontroli ne adminiem
+    unless @is_admin
+      if @user_params.id != current_user.id
+        render_404
+      end
+    end
       
-    @field = Field.find(@user.golf_club.id)
-    @user_sticks = UsersStick.where(:user_id => @user.id).order("stick.stick_type ASC")
-    @statistic = Statistic.where(:user_id => @user.id, :stick_id => @user_sticks.id).order("stick.stick_type ASC")
-   
-    
+    @user_sticks = UsersStick.where(:user_id => @user_params.id).order("stick.stick_type ASC")
+    @statistic = Statistic.where(:user_id => @user_params.id, :stick_id => @user_sticks.id).order("stick.stick_type ASC")
   
-
-
-    
-
   end
 
   def update
