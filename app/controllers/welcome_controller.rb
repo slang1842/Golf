d730fcdb55ee0c356b1bba417a3d0f1e92f9321d
ExEdzy <edgars.liepa@creo.mobi>
2011-06-07 14:39:50 +0300

    sync

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 before_filter :require_user, :only => :index
 before_filter :require_no_user, :only => :welcome
 skip_before_filter :header
-
+before_filter :require_no_super_admin, :only => :index
 
   def index
     store_location
