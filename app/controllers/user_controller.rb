c13e659bbd53b68de642adebaa4d4cdb96fd154d
ExEdzy <edgars.liepa@creo.mobi>
2011-06-10 11:51:20 +0300

    user registration validation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -35,10 +35,8 @@ class UserController < ApplicationController
         end
       else
         #format.html { render :action => "new" }
-        #format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
         format.html { render :action => "new" }
-        #redirect_to new_user_path
-        #render :action => "new"
       end
     end
   end
