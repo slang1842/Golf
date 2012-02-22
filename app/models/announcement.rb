11b06011e5728b73cb2ff0f3e7c347424a880ceb
janiscaunecm <jca@inbox.lv>
2012-02-23 01:56:58 +0200

    Starting to port to pg

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -8,15 +8,15 @@ class Announcement < ActiveRecord::Base
               :path => ":rails_root/public/announcement/:attachment/:id_:style.:extension"
 
 
-	scope :for_newsfeed, lambda {|limit, offset, club_id_array| where("announcements.golf_club_id IN(?)", club_id_array).limit(limit).offset(offset).order("announcements.created_at desc")}
+	scope :for_newsfeed, lambda {|limit, offset, club_id_array| where('announcements.golf_club_id IN(?)', club_id_array).limit(limit).offset(offset).order('announcements.created_at desc')}
 
-	scope :latest, lambda {|limit, club_id| where("announcements.golf_club_id IN(?)", [club_id, ADMIN_GOLF_CLUB_ID]).limit(limit).order("announcements.created_at desc")}
+	scope :latest, lambda {|limit, club_id| where('announcements.golf_club_id IN(?)', [club_id, ADMIN_GOLF_CLUB_ID]).limit(limit).order('announcements.created_at desc')}
 
-	scope :by_single_club, lambda {|club_id| where("announcements.golf_club_id = ?", club_id).order("announcements.created_at desc")}
+	scope :by_single_club, lambda {|club_id| where('announcements.golf_club_id = ?', club_id).order('announcements.created_at desc')}
 
-	scope :my_feed, lambda {|limit, offset, club_id| where("announcements.golf_club_id = ?", club_id).offset(offset).limit(limit).order("announcements.created_at desc")}
+	scope :my_feed, lambda {|limit, offset, club_id| where('announcements.golf_club_id = ?', club_id).offset(offset).limit(limit).order('announcements.created_at desc')}
 
-	scope :by_admin, lambda {|limit, offset| where("announcements.golf_club_id = ?", ADMIN_GOLF_CLUB_ID).limit(limit).offset(offset).order("announcements.created_at desc")}
+	scope :by_admin, lambda {|limit, offset| where('announcements.golf_club_id = ?', ADMIN_GOLF_CLUB_ID).limit(limit).offset(offset).order('announcements.created_at desc')}
 
 	def self.convert_to_header(full_text, header_length)
 		header = truncate(full_text, header_length, "...")
