825a62e1a9248052b50b08a48526d6ffc6d05ac7
janiscaunecm <jca@inbox.lv>
2012-02-23 04:35:04 +0200

    More changes to migrations

--- db/migrate/20111227122617_change_data_type_for_distances.rb
+++ db/migrate/20111227122617_change_data_type_for_distances.rb
@@ -1,26 +1,35 @@
 class ChangeDataTypeForDistances < ActiveRecord::Migration
    def self.up
     change_table :hits do |t|
-			t.change :distance_to_hole, :float
-      t.change :hit_distance, :float
+			t.remove_column :distance_to_hole
+      t.remove_column :hit_distance
+			t.add_column :distance_to_hole, :float
+      t.add_column :hit_distance, :float
     end
 		change_table :sticks do |t|
-			t.change :distance, :float
+			t.remove_column :distance
+			t.add_column :distance, :float
 		end
 		
 		change_table :holes do |t|
-			t.change :distance, :float
+			t.remove_column :distance
+			t.add_column :distance, :float
 		end
 
 		change_table :fields do |t|
-			t.change :very_short_distance, :string
-      t.change :short_distance, :string
-     	t.change :normal_distance, :string
-   		t.change :long_distance, :string
+			t.remove_column :very_short_distance
+      t.remove_column :short_distance
+     	t.remove_column :normal_distance
+   		t.remove_column :long_distance
+			t.add_column :very_short_distance, :string
+      t.add_column :short_distance, :string
+     	t.add_column :normal_distance, :string
+   		t.add_column :long_distance, :string
 		end
 
 		change_table :users_sticks do |t|
-			t.change :distance, :float
+			t.remove_column :distance
+			t.add :distance, :float
 		end
   end
 
