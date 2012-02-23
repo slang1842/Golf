f3784eb2b238aeff7ad6969381493e67afc93019
janiscaunecm <jca@inbox.lv>
2012-02-23 03:37:41 +0200

    Removed migration

--- db/migrate/20120221170202_add_short_name_column_to_golf_clubs.rb
+++
@@ -1,9 +0,0 @@
-class AddShortNameColumnToGolfClubs < ActiveRecord::Migration
-  def self.up
-		add_column :golf_clubs, :short_name, :string
-		add_column :fields, :short_name, :string
-  end
-
-  def self.down
-  end
-end
