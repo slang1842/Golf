6ee31e682b6501404dd138a5b50f777c9799f6ad
janiscaunecm <jca@inbox.lv>
2012-02-23 02:27:27 +0200

    Seeding countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,46 +11,9 @@
 
 
 lv = Country.create(:name => 'Latvia')
-it = Country.create(:name => 'Italy')
+lit = Country.create(:name => 'Lithuania')
 
 
-Stick.create(:id => 1,
-  :stick_type => "PUTTER",
-  :distance => "210",
-  :degrees => "34",
-  :shaft => "wood",
-  :short_name => "P",
-  :shaft_strength => "strong")
-              
-Stick.create(:id => 2,
-  :stick_type => 'ORIONIONIN',
-  :distance => 100,
-  :degrees => 35,
-  :shaft => "metal",
-  :short_name => "O",
-  :shaft_strength => "strong")
-              
 
-              
-Stick.create(:id => 3,
-  :stick_type => "DRIVER",
-  :distance => "160",
-  :degrees => "14",
-  :shaft => "metal",
-  :short_name => "D",
-  :shaft_strength => "very strong")
-              
-Stick.create(:id => 4,
-  :stick_type => "WOODBOW",
-  :distance => "410",
-  :degrees => "45",
-  :shaft => "wood",
-  :short_name => "W",
-  :shaft_strength => "medium strong")
-
-HitPlace.create(:name => "Red")
-HitPlace.create(:name => "Green")
-HitPlace.create(:name => "Black")
-HitPlace.create(:name => "Yellow")
 
 
